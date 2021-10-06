2ec744e6f2ac6d3a553d5f4b9d8f943c47d619d3
Phanie Buonomo <fanny.buonomo@insa-lyon.fr>
2021-10-06 21:53:42 +0200

    created Class to test SkipList

---
+++ TestSkipList.java
@@ -0,0 +1,79 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public final class TestSkipList {
+
+  public enum Setting {
+    OPERATIONS,
+    THREADS,
+    ADDS,
+    REMOVES,
+    CONTAINS
+      ;
+
+  }
+  private int operations, threads;
+  private double adds, removes, contains;
+  private SkipList sl;
+  private ExecutorService executorService;
+
+  public TestSkipList(SkipList sl) {
+    this.sl = sl;
+  }
+  
+  public void set(int op, int t, double a, double r, double c) {
+    operations = op;
+    threads = t;
+    adds = a;
+    removes = r;
+    contains = c;
+  }
+
+  public void set(Setting s, double value) {
+    switch(s) {
+      case OPERATIONS:
+        operations = (int) value;
+        break;
+      case THREADS:
+        threads = (int) value;
+        break;
+      case ADDS:
+        adds = value;
+        break;
+      case REMOVES:
+        removes = value;
+        break;
+      case CONTAINS:
+        contains = value;
+        break;
+      default:
+    }
+  }
+  
+  public void stop(){
+    executorService.shutdown();
+  }
+
+  public void start() {
+    executorService = Executors.newFixedThreadPool(threads);
+    int opPerThread = operations/threads; //approximation +/- 1 can be ignored here
+    for(int i = 0; i < threads; i++) {
+      executorService.submit(new Runnable() {
+          public void run() {
+            for(int a = 0; a < opPerThread*adds; a++){
+              sl.add((int)(Math.random()*adds));
+            }
+
+            for(int r = 0; r < opPerThread*removes; r++){
+              sl.remove((int)(Math.random()*removes));
+            }
+          
+            for(int c = 0; c < opPerThread*contains; c++){
+              sl.contains((int)(Math.random()*contains));
+            }
+          }
+      });
+    }
+  }
+}
